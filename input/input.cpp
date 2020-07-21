890d676ae592598834680725538689a39a938fc0
Gaks98 <yvonnegax98@gmail.com>
2020-07-22 01:53:56 +0300

    add input

---
+++ input/input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+  int slices;
+  cout << "Enter number of slices\n";
+  cin >> slices;
+  cout << "There are only " << slices << " slices left" << endl;
+  return 0;
+}
+
